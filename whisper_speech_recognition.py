d3c42d91b164be82748fa21c3fe49740d06198e0
IbrahimLakhzine <46730171+IbrahimLakhzine@users.noreply.github.com>
2022-09-29 11:52:50 +0100

    adding whisper_speech_recognition file

---
+++ whisper_speech_recognition.py
@@ -0,0 +1,33 @@
+import whisper
+
+model = whisper.load_model("base")
+result = model.transcribe("C:/Users/ibrahim.l/Desktop/whisper_speech_recognition/hala.mp3")
+print(result["text"])
+
+
+
+
+"****************************************************************************************"
+"""
+import whisper
+
+model = whisper.load_model("base")
+
+# load audio and pad/trim it to fit 30 seconds
+audio = whisper.load_audio("C:/Users/ibrahim.l/Desktop/stay.mp3")
+audio = whisper.pad_or_trim(audio)
+
+# make log-Mel spectrogram and move to the same device as the model
+mel = whisper.log_mel_spectrogram(audio).to(model.device)
+
+# detect the spoken language
+_, probs = model.detect_language(mel)
+print(f"Detected language: {max(probs, key=probs.get)}")
+
+# decode the audio
+options = whisper.DecodingOptions()
+result = whisper.decode(model, mel, options)
+
+# print the recognized text
+print(result.text)
+"""
